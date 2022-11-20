12949eb529b8e1da2acebdd234e44f49b04db58c
Sergey Fadeev <artistsilen@gmail.com>
2022-11-20 22:14:04 +0300

    quality done

--- src/App/components/qualitiy.jsx
+++ src/App/components/qualitiy.jsx
@@ -2,18 +2,15 @@ import { queryAllByTestId } from "@testing-library/react";
 import React from "react";
 
 function Quality(props) {
-  console.log(
-    "Quality",
-    Object.values(props).map((qual) => qual.name)
+  return (
+    <>
+      {Object.values(props).map((qual) => (
+        <button key={qual._id} className={"badge primary m-2 bg-" + qual.color}>
+          {qual.name}
+        </button>
+      ))}
+    </>
   );
-
-  Object.values(props).map((qual) => {
-    return (
-      <button key={qual._id} className={"badge primary m-2 bg-" + qual.color}>
-        {qual.name}
-      </button>
-    );
-  });
 }
 
 export default Quality;
